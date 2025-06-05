08c5f2fd34ab777d361369a54cdae93d317a6ee9
NACHITOO419 <gazzaneonacho@gmail.com>
2025-06-04 23:53:08 -0300

    Codigo base JS

--- Script.js
+++ Script.js
@@ -1,1 +1,13 @@
-ECHO est  activado.
+//Declaramos las variables que vamos a utilizar....
+let Resul = 0;//R
+let Num1 = 0;//J
+let Num2 = 1;//K
+
+//Declaro ciclo for que va a : Sumar -Actualizar - mostrar ...
+for(let i=0;i<5;i++){
+    Resul=Num1 + Num2;
+    Num1=Num2;
+    Num2=Resul;
+        alert(Resul);
+        console.log(Num1, Num2);
+}
