8957acc658a7792b29cdf610965cbe4800ff0c00
NACHITOO419 <gazzaneonacho@gmail.com>
2025-06-06 01:41:59 -0300

    cambios en la muestra de la serie

--- Script.js
+++ Script.js
@@ -1,13 +1,24 @@
+//Declaracion de objetos Html (DOM)...
+const BtNiniS = document.getElementById("IniS");
 //Declaramos las variables que vamos a utilizar....
-let Resul = 0;//R
+/*let Resul = 0;//R
 let Num1 = 0;//J
-let Num2 = 1;//K
-
+let Num2 = 1;//K*/
+//Declaro evento click para el boton...
+//-Cuando se haga click en el boton se ejucutara el for-
+BtNiniS.addEventListener("click",function(){
 //Declaro ciclo for que va a : Sumar -Actualizar - mostrar ...
-for(let i=0;i<5;i++){
-    Resul=Num1 + Num2;
-    Num1=Num2;
-    Num2=Resul;
-        alert(Resul);
-        console.log(Num1, Num2);
-}
+        let Resul = 0;//R
+        let Num1 = 0;//J
+        let Num2 = 1;//K
+    
+    for(let i=0;i<5;i++){
+        Resul=Num1 + Num2;
+        Num1=Num2;
+        Num2=Resul;
+            document.writeln("Resultado: ",Resul +"<br>"); 
+            /*alert(Resul);// aca lo comentetmos porque pusemos un boton a esperar el click para que realice la funcion.
+            console.log(Num1, Num2);*/
+    
+          }})
+//el for Por completo va dentro de los parametros de la funcion...
